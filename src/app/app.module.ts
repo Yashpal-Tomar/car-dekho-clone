040dbcd6cb2c3e5e489b818cbe3d3f8e0f31f44d
Yashpal Tomar <yashutomar699@gmail.com>
2022-08-29 20:41:05 +0530

    Added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import { NgSelectModule } from '@ng-select/ng-select';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared/shared.module';
+import { FooterComponent } from './shared/layout/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgSelectModule,
     FormsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
